8693aff967bc3c9ecce02c562a3bb77b4c63b589
CrioUser <crio-user@criodo.com>
2022-08-01 08:43:20 +0530

    ME_QCALC_V2_MODULE_TESTSANDERRORS

--- src/main/java/com/crio/qcalc/StandardCalculator.java
+++ src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -40,16 +40,26 @@ public class StandardCalculator {
     }
 
     
+    
     public void add(double num1, double num2){
-        //add((double) num1, (double) num2);
-        result = num1 + num2;
+        double result = num1 + num2;
+        if((result == Double.MAX_VALUE) || (result == Double.POSITIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
     }
 
+
+    
     
     public void subtract(double num1, double num2){
-        //subtract((double) num1, (double) num2);
-        result = num1 - num2;
+        double result = num1 - num2;
+        if((result == -Double.MAX_VALUE) || (result == Double.NEGATIVE_INFINITY)){
+            throw new ArithmeticException("Double overflow");
+        }
+        this.result = result;
     }
+
 
 
     public void multiply(double num1, double num2){
